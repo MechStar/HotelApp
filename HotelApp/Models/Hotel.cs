8626860c86926a6b7e75c17a5932d26949707e96
Denis Pavlov <denis.pavlov@dial800.com>
2019-06-13 19:51:16 -0700

    Simplify The Code

--- HotelApp/Models/Hotel.cs
+++ HotelApp/Models/Hotel.cs
@@ -4,6 +4,25 @@
     {
         public long Guests { get; set; }
         public long Rooms { get; set; }
-        public bool[] RoomLights => Utilities.Electrical.GetRoomLights(Guests, Rooms);
+        public bool[] RoomLights => GetRoomLights(Guests, Rooms);
+
+        private bool[] GetRoomLights(long guests, long rooms)
+        {
+            bool[] roomLights = new bool[rooms];
+
+            for (long guestNum = 1; guestNum <= guests; guestNum++)
+            {
+                long flipsByPerson = rooms / guestNum;
+
+                for (long flip = 1; flip <= flipsByPerson; flip++)
+                {
+                    long roomNum = guestNum * flip - 1;
+
+                    roomLights[roomNum] = !roomLights[roomNum];
+                }
+            }
+
+            return roomLights;
+        }
     }
 }
