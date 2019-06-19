43e61e536598a5a69d30dc4cf252b837f0e4f85f
Denis Pavlov <denis.pavlov@dial800.com>
2019-06-18 20:43:28 -0700

    Add Required Validation

--- HotelApp/App_Start/FilterConfig.cs
+++ HotelApp/App_Start/FilterConfig.cs
@@ -1,4 +1,4 @@
-﻿using HotelApp.Attributes;
+﻿using HotelApp.Filters;
 using System.Web.Mvc;
 
 namespace HotelApp
--- HotelApp/Filters/LogAndHandleError.cs
+++ HotelApp/Filters/LogAndHandleError.cs
@@ -1,6 +1,6 @@
 ﻿using System.Web.Mvc;
 
-namespace HotelApp.Attributes
+namespace HotelApp.Filters
 {
     public class LogAndHandleError : HandleErrorAttribute
     {
