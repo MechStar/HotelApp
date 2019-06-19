67e0bfdc585ca5468e1e6dc498e7601983308870
Denis Pavlov <denis.pavlov@dial800.com>
2019-06-18 17:52:27 -0700

    Split GET/POST Actions And Add Defaults

--- HotelApp/Controllers/HotelController.cs
+++ HotelApp/Controllers/HotelController.cs
@@ -1,10 +1,21 @@
 ﻿using HotelApp.Models;
+using System.Configuration;
 using System.Web.Mvc;
 
 namespace HotelApp.Controllers
 {
     public class HotelController : Controller
     {
+        [HttpGet]
+        public ActionResult CountLights()
+        {
+            var defaultGuests = long.Parse(ConfigurationManager.AppSettings["DefaultGuests"]);
+            var defaultRooms = long.Parse(ConfigurationManager.AppSettings["DefaultRooms"]);
+
+            return View(new Hotel() { Guests = defaultGuests, Rooms = defaultRooms });
+        }
+
+        [HttpPost]
         public ActionResult CountLights(Hotel model)
         {
             return View(model);
