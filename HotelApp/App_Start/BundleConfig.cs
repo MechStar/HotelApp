9f6a4046ddeb1f32771965b5ba687b54c6f37a22
Denis Pavlov <denis.pavlov@dial800.com>
2019-06-18 19:25:18 -0700

    Enable Bundle Minification

--- HotelApp/App_Start/BundleConfig.cs
+++ HotelApp/App_Start/BundleConfig.cs
@@ -25,6 +25,12 @@ namespace HotelApp
             bundles.Add(new StyleBundle("~/Content/css").Include(
                         "~/Content/bootstrap.css",
                         "~/Content/site.css"));
+
+#if DEBUG
+            BundleTable.EnableOptimizations = false;
+#else
+            BundleTable.EnableOptimizations = true;
+#endif
         }
     }
 }
